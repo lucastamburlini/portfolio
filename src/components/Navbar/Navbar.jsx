84c8d56660ef09c48187966cf4849dd67579f3b6
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-14 20:31:21 -0300

    [fix] estilos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,39 +30,41 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={style.navbar + " " + styleApp.container}>
-      <div className={style.navbarLogo}>
-        <img src={logoWhiteLarge} alt="logo_white_large.png" />
-      </div>
-      <div className={style.navbarAssets}>
-        <LanguageSwitch />
-        <div className={`${style.navbarLinks} ${open ? style.active : ""}`}>
-          <div>
-            <ul>
-              <li>
-                <a href="#about" onClick={handleClick}>
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#skills" onClick={handleClick}>
-                  Skills
-                </a>
-              </li>
-              <li>
-                <a href="#projects" onClick={handleClick}>
-                  Projects
-                </a>
-              </li>
-            </ul>
-          </div>
+    <nav>
+      <div className={styleApp.container + " " + style.navContainer}>
+        <div className={style.navbarLogo}>
+          <img src={logoWhiteLarge} alt="logo_white_large.png" />
         </div>
-        <div className={style.navbarToggle} onClick={handleClick}>
-          {open ? (
-            <CloseIcon className={style.menuClose} />
-          ) : (
-            <MenuIcon className={style.menuOpen} />
-          )}
+        <div className={style.navbarAssets}>
+          <LanguageSwitch />
+          <div className={`${style.navbarLinks} ${open ? style.active : ""}`}>
+            <div>
+              <ul>
+                <li>
+                  <a href="#about" onClick={handleClick}>
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a href="#skills" onClick={handleClick}>
+                    Skills
+                  </a>
+                </li>
+                <li>
+                  <a href="#projects" onClick={handleClick}>
+                    Projects
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={style.navbarToggle} onClick={handleClick}>
+            {open ? (
+              <CloseIcon className={style.menuClose} />
+            ) : (
+              <MenuIcon className={style.menuOpen} />
+            )}
+          </div>
         </div>
       </div>
     </nav>
