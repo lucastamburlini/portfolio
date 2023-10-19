725e7f83e04ce1b329ec820998c190532ad100c4
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-18 21:20:05 -0300

    style(about): add img and text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,27 @@
 import { useSelector } from "react-redux";
 import translations from "../../assets/translations";
+import imgAbout from "../../assets/imgAbout.jpg";
 
-import style from './About.module.css';
+import style from "./About.module.css";
 
 const About = () => {
   const currentLanguage = useSelector((state) => state.language);
+  const currentTranslations = translations[currentLanguage];
 
   return (
     <div id="about" className={style.about}>
-      <h2>{translations[currentLanguage].about_title}</h2>
-      <p>{translations[currentLanguage].about_text_I}</p> <br />
-      <p>{translations[currentLanguage].about_text_II}</p>
-      <br />
-      <p>{translations[currentLanguage].about_text_III}</p>
+      <section className={style.aboutImg}>
+        <img src={imgAbout} alt={imgAbout} />
+      </section>
+      <section className={style.aboutInfo}>
+        <header>
+          <h2>{currentTranslations.about_title}</h2>
+        </header>
+        <main>
+          <p>{currentTranslations.about_text_I}</p>
+          <p>{currentTranslations.about_text_II}</p>
+        </main>
+      </section>
     </div>
   );
 };
